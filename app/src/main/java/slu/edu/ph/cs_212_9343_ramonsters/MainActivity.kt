83594ce164d5f59fc1beb0f7f4a0f3731a8ae204
Rithik Tank <2233293@slu.edu.ph>
2023-11-21 20:12:22 +0800

    Made rough code for login

--- app/src/main/java/slu/edu/ph/cs_212_9343_ramonsters/MainActivity.kt
+++ app/src/main/java/slu/edu/ph/cs_212_9343_ramonsters/MainActivity.kt
@@ -2,10 +2,45 @@ package slu.edu.ph.cs_212_9343_ramonsters
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var logInButton: Button
+    lateinit var signUpButton : Button
+    lateinit var emailField: EditText
+    lateinit var passwordField : EditText
+    lateinit var email : String
+    lateinit var password : String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        logInButton = findViewById(R.id.login)
+        signUpButton = findViewById(R.id.SignUpButton)
+        emailField = findViewById(R.id.EmailAddressField)
+
+
+
+
+        logInButton.setOnClickListener {
+            //email = emailField.getText().toString()
+            //password = passwordField.getText().toString()
+            setContentView(R.layout.activity_main2)
+        }
+
+        signUpButton.setOnClickListener {
+            email = emailField.getText().toString()
+            password = passwordField.getText().toString()
+
+        }
+
+
+
+
     }
 }
