38f8aaf45d163f8923ea2976529e5dc583ded165
Rithik Tank <2233293@slu.edu.ph>
2023-11-22 17:59:41 +0800

    Made initial code for login

--- app/src/main/java/slu/edu/ph/cs_212_9343_ramonsters/LoginSignUp.kt
+++ app/src/main/java/slu/edu/ph/cs_212_9343_ramonsters/LoginSignUp.kt
@@ -2,12 +2,10 @@ package slu.edu.ph.cs_212_9343_ramonsters
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import org.w3c.dom.Text
 
-class MainActivity : AppCompatActivity() {
+class LoginSignUp : AppCompatActivity() {
 
     lateinit var logInButton: Button
     lateinit var signUpButton : Button
@@ -18,24 +16,27 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.loginsignup)
 
         logInButton = findViewById(R.id.login)
         signUpButton = findViewById(R.id.SignUpButton)
+        passwordField = findViewById(R.id.EnterPasswordField)
         emailField = findViewById(R.id.EmailAddressField)
 
 
-
-
         logInButton.setOnClickListener {
-            //email = emailField.getText().toString()
-            //password = passwordField.getText().toString()
-            setContentView(R.layout.activity_main2)
+            email = emailField.getText().toString()
+            password = passwordField.getText().toString()
+
+            if (email == "tutor@email.com" && password == "tutor123") {
+                setContentView(R.layout.tutormenu)
+            } else if (email == "student@email.com" && password == "student123")
+                setContentView(R.layout.studentmenu)
         }
 
         signUpButton.setOnClickListener {
-            email = emailField.getText().toString()
-            password = passwordField.getText().toString()
+            email = emailField.text.toString()
+            password = passwordField.text.toString()
 
         }
 
--- app/src/main/java/slu/edu/ph/cs_212_9343_ramonsters/TutorMenu.kt
+++ app/src/main/java/slu/edu/ph/cs_212_9343_ramonsters/TutorMenu.kt
@@ -2,12 +2,11 @@ package slu.edu.ph.cs_212_9343_ramonsters
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button;
 
-class MainActivity2 : AppCompatActivity() {
+class TutorMenu : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.tutormenu)
     }
 }
